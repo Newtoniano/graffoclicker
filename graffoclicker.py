bbdd998b01ea7c88bb3fc1033a565a71d956053b
Newtoniano <81423796+Newtoniano@users.noreply.github.com>
2022-12-16 22:07:54 +0100

    improve duration prompt

--- graffoclicker.py
+++ graffoclicker.py
@@ -1,4 +1,5 @@
 import random
+import sys
 import time
 
 import pyautogui
@@ -28,38 +29,45 @@ startBot = pyautogui.confirm(
     "Do you want to start Graffoclicker? Please be on the task window when you start."
 )
 if startBot == "OK":
+    targetDuration = pyautogui.prompt(
+        "Insert desired duration in minutes. Default if no value inserted: 1 hour.",
+    )
+    if targetDuration is None:
+        print("Graffoclicker execution stopped.")
+        pyautogui.alert("Graffoclicker execution stopped.")
+        sys.exit()
+    elif targetDuration == "":  # behavior when you press ok with no input
+        targetDuration = 60 * 60  # defaults to 1 hour
+    elif targetDuration:
+        targetDuration = int(targetDuration) * 60  # convert minutes to seconds
+
     # TODO: runtime stats overlay
-    targetDuration = int(pyautogui.prompt("Insert desired duration in minutes")) * 60
-    if targetDuration:
-        # Make window active again after clicking on the prompt
-        pyautogui.click()
 
-        tasksDone = 0
-        startTime = time.time()
+    # Make window active again after clicking on the prompt
+    pyautogui.click()
 
-        try:
-            while True:
-                currentTime = time.time()
-                if currentTime - startTime > targetDuration:
-                    break
-                taskClicker()
-                tasksDone += 1
-                pyautogui.hotkey("alt", "tab")
-                time.sleep(
-                    random.randint(30, 180)
-                )  # Random 30s-3m delay between each run
-                pyautogui.hotkey("alt", "tab")
+    tasksDone = 0
+    startTime = time.time()
 
-            print(
-                f"Graffoclicker finished running at {time.localtime()} after {targetDuration} minutes. {tasksDone} tasks have been submitted."
-            )
-            pyautogui.alert(
-                f"Graffoclicker finished running at {time.localtime()} after {targetDuration} minutes. {tasksDone} tasks have been submitted."
-            )
-        except KeyboardInterrupt:
-            print(
-                f"Graffoclicker stopped early. {tasksDone} tasks have been submitted."
-            )
-            pyautogui.alert(
-                f"Graffoclicker stopped early. {tasksDone} tasks have been submitted."
-            )
+    try:
+        while True:
+            currentTime = time.time()
+            if currentTime - startTime > targetDuration:
+                break
+            taskClicker()
+            tasksDone += 1
+            pyautogui.hotkey("alt", "tab")
+            time.sleep(random.randint(30, 180))  # Random 30s-3m delay between each run
+            pyautogui.hotkey("alt", "tab")
+
+        print(
+            f"Graffoclicker finished running at {time.localtime()} after {targetDuration/60} minutes. {tasksDone} tasks have been submitted."
+        )
+        pyautogui.alert(
+            f"Graffoclicker finished running at {time.localtime()} after {targetDuration/60} minutes. {tasksDone} tasks have been submitted."
+        )
+    except KeyboardInterrupt:
+        print(f"Graffoclicker stopped early. {tasksDone} tasks have been submitted.")
+        pyautogui.alert(
+            f"Graffoclicker stopped early. {tasksDone} tasks have been submitted."
+        )
